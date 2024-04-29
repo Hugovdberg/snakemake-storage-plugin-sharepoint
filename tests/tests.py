53f5d033d8f11456b87853e4811ba31c13e1dda1
Hugo Lapré <hugo.lapre@brabantwater.nl>
2024-04-29 10:47:36 +0200

    chore: code style fixes

--- tests/tests.py
+++ tests/tests.py
@@ -4,15 +4,18 @@ __email__ = "johannes.koester@uni-due.de"
 __license__ = "MIT"
 
 from typing import List, Optional, Type
-from snakemake_interface_storage_plugins.tests import TestStorageBase
-from snakemake_interface_storage_plugins.storage_provider import StorageProviderBase
+
 from snakemake_interface_storage_plugins.settings import StorageProviderSettingsBase
+from snakemake_interface_storage_plugins.storage_provider import StorageProviderBase
+from snakemake_interface_storage_plugins.tests import TestStorageBase
+
 from snakemake_storage_plugin_sharepoint import StorageProvider, StorageProviderSettings
 
 
 class TestStorageNoSettings(TestStorageBase):
-    __test__ = False  # for now no way to test this as we don't have a way to mock the SharePoint server
-    retrieve_only = True
+    # For now there is no way to test this package as we don't have a way to mock a
+    # SharePoint server, therefore we set __test__ to False
+    __test__ = False
 
     def get_query(self, tmp_path) -> str:
         return "stable"
